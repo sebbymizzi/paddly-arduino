3174018d923f4513e0c13be79713e9bbc0c30ff7
Nicole Grech <nicolegrech@Nicoles-MacBook-Pro.local>
2017-06-03 15:30:06 +0200

    arduino to binary

--- arduino_binary/sketch/sketch.ino
+++ arduino_binary/sketch/sketch.ino
@@ -1,26 +1,31 @@
+int openLock = 5;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);;
-  pinMode(22, OUTPUT);
-  pinMode(23, OUTPUT);
-  pinMode(24, OUTPUT);
-  pinMode(25, OUTPUT);
-  pinMode(26, OUTPUT);
-  pinMode(27, OUTPUT);
-  pinMode(28, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-   openLocker(22, 5);
+   openLocker(2, openLock);
 }
 
 void openLocker(int from_pin, int locker_no) {
-   digitalWrite(from_pin, HIGH && (locker_no & B01000000));
-   digitalWrite(from_pin+1, HIGH && (locker_no & B00100000));
-   digitalWrite(from_pin+2, HIGH && (locker_no & B00010000));
+   digitalWrite(from_pin+7, HIGH && (locker_no & B10000000));
+   digitalWrite(from_pin+6, HIGH && (locker_no & B01000000));
+   digitalWrite(from_pin+5, HIGH && (locker_no & B00100000));
+   digitalWrite(from_pin+4, HIGH && (locker_no & B00010000));
    digitalWrite(from_pin+3, HIGH && (locker_no & B00001000));
-   digitalWrite(from_pin+4, HIGH && (locker_no & B00000100));
-   digitalWrite(from_pin+5, HIGH && (locker_no & B00000010));
-   digitalWrite(from_pin+6, HIGH && (locker_no & B00000001));
+   digitalWrite(from_pin+2, HIGH && (locker_no & B00000100));
+   digitalWrite(from_pin+1, HIGH && (locker_no & B00000010));
+   digitalWrite(from_pin, HIGH && (locker_no & B00000001));
 }
+
